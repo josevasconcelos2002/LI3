d4f736d0edf5a6803072a34ad9b9af412daba4cf
José Pedro Torres Vasconcelos <a100763@alunos.uminho.pt>
2022-10-21 20:01:35 +0100

    Algumas adições no parsing

--- src/parse.c
+++ src/parse.c
@@ -5,24 +5,33 @@
     FILE *file;
     open = fopen(file_name,"r");
     if(file_name = "user.csv"){
+        char *user,*name,*birth_date,*account_creation, *pay_method, *account_status;
         if(file == NULL) printf("O respetivo ficheiro está vazio.\n");
-    
+        while((fscanf(file,"%s;%s;%s;%s;%s;%s;%s\n",&user,&name,&birth_date,&account_creation,&pay_method,&account_status))!=EOF){
+            /*Enquanto a leitura do ficheiro for válida , de acordo com os tipos esperados, guarda os users na hashtable*/
+        }
 
         fclose(file);
-        return /*users_hashtable*/;
+        return /*users_hashtable já preenchida*/;
     }
     else if(file_name = "drivers.csv"){
+        int id;
+        char *name,*birth_date,*gender,*car_class,*license_plate,*city,*account_creation,*account_status;
         if(file == NULL) printf("O respetivo ficheiro está vazio.\n");
-    
+        while((fscanf(file,"%d;%s;%s;%s;%s;%s;%s\n",&id,&name,&birth_date,&gender,&car_class,&license_plate,&city,&account_creation,&account_status))!=EOF){
+            /*Enquanto a leitura do ficheiro for válida , de acordo com os tipos esperados, guarda os drivers na hashtable*/
+        }
 
         fclose(file);
-        return /*drivers_hashtable*/;
+        return /*drivers_hashtable já preenchida*/;
     }
     else if(file_name = "rides.csv") {
+        int id;
+        char *date,*driver,*user,*city; distance; score_user; score_driver;tip; comment
         if(file == NULL) printf("O respetivo ficheiro está vazio.\n");
     
 
         fclose(file);
-        return /*rides_hashtable*/;
+        return /*rides_hashtable já preenchida*/;
     }
 }
