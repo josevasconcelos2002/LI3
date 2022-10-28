981d31e1f16a248220a53530f80eae4b600c631a
José Pedro Torres Vasconcelos <a100763@alunos.uminho.pt>
2022-10-28 16:54:20 +0100

    Atualzei uns ficheiros

--- src/drivers.c
+++ src/drivers.c
@@ -4,39 +4,74 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "drivers.h"
+#include <glib-2.0>
 
-#define TABLE_SIZE 100 /*how much?*/
+#define TABLE_SIZE 1000 /*how much?*/
 
-/*retorna a hashtable resultante?*/User *parse(const char* file_name){
-    FILE *file;
-    FILE* open = fopen(file_name,"r");
-    if(file_name == "drivers.csv"){
-        Driver d;
-        if(file == NULL) printf("O respetivo ficheiro está vazio.\n");
-        while((fscanf(file,"%d;%s;%s;%s;%s;%s;%s\n",&d.id,&d.name,&d.birth_date,&d.gender,&d.car_class,&d.license_plate,&d.city,&d.account_creation,&d.account_status))!=EOF){
-            /*Enquanto a leitura do ficheiro for válida , de acordo com os tipos esperados, guarda os drivers na hashtable*/
-            insert_driver();
-            drivers->next;      
-        }
-        fclose(file);
-        return /*users_hashtable já preenchida*/;
+User *hash_table_drivers[TABLE_SIZE];
+
+void init_drivers_table()
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+        hash_table_drivers[i] = NULL;
+}
+
+void *parse_driver(const char *file_name)
+{
+    FILE *open = NULL;
+    open = fopen(file_name, "r");
+    char *info;
+    int line = 0;
+    int count = 0;
+    Driver d;
+    if (open == NULL)
+        printf("O respetivo ficheiro está vazio.\n");
+    while ((fgets(info,sizeof(info),open))){
+          line ++;
+          char *field = strtok(info,";");                           // alternativa usando o 'fgets' que, pelos vistos, é mais aconselhado
+          while (field)
+          {
+            if(count == 0) strcpy(d.id,field);
+            if(count == 1) strcpy(d.name,field);
+            if(count == 2) strcpy(d.birth_date,field);
+            if(count == 3) strcpy(d.gender,field);
+            if(count == 4) strcpy(d.car_class,field);
+            if(count == 5) strcpy(d.license_plate,field);
+            if(count == 6) strcpy(d.city,field);
+            if(count == 7) strcpy(d.account_creation,field);
+            if(count == 8) strcpy(d.account_status,field);
+            field = strtok(NULL,";");
+            count ++;
+          }    
     }
+    /*while ((fscanf(open,"%s;%s;%s;%s;%s;%s;%s\n",&u.username,&u.name,&u.gender,&u.birth_date,&u.account_creation,&u.pay_method,&u.account_status)) != EOF)
+    {
+        insert_user(&user);
+    }*/
+    fclose(open); /*users_hashtable já preenchida*/;
+}
 
 Driver *drivers_hash_table[TABLE_SIZE];
 
-void init_hash_table(){
+void init_drivers_table(){
     for(int i = 0;i<TABLE_SIZE;i++) drivers_hash_table[i] = NULL;
 }
 
-bool valid_driver(Driver *d){
-    if(d == NULL) return false;
-    if((strcmp(d->gender,"M") && strcmp(d->gender,"F") != 0) return false;
-    if(strlen(d->birth_date) != 10) return false;
-    if((strlen(d->license_plate != 8) return false;
-    if(strlen(d->account_creation) != 10) return false;
-    if(((strcmp(d->car_class,"basic") && strcmp(d->car_class,"green") && strcmp(d->car_class,"premium")) != 0)) return false;
-    if(((strcmp(d->account_status,"active")) && strcmp(d->account_status,"inactive"))!= 0) return false; 
-    else return true;
+bool valid_drivers(Driver *d){
+    if(!d) 
+        return false;
+    if((strcmp(d->gender,"M") && strcmp(d->gender,"F") != 0))
+        return false;
+    if ((strlen(d->birth_date) || strlen(d->account_creation)) != 10)
+        return false;
+    if((strlen(d->license_plate != 8)))
+        return false;
+    if(((strcmp(d->car_class,"basic") && strcmp(d->car_class,"green") && strcmp(d->car_class,"premium")) != 0)) 
+        return false;
+    if(((strcmp(d->account_status,"active")) && strcmp(d->account_status,"inactive"))!= 0)
+        return false; 
+    else
+        return true;
 }
 
 
@@ -44,4 +79,11 @@ bool insert_driver(Driver *d){
     if(valid_driver(d)){
         /*falta inserir*/
     }
+}
+
+int main(int argv, char **argc){
+        init_drivers_table();
+        print_drivers_table();
+        parse_drivers("drivers.csv");
+        print_drivers_table();
 }
--- src/users.c
+++ src/users.c
@@ -9,9 +9,9 @@
 
 #define TABLE_SIZE 1000 /*how much?*/
 
-User *hash_table[TABLE_SIZE];
+User *hash_table_users[TABLE_SIZE];
 
-void init_hash_table()
+void init_users_table()
 {
     for (int i = 0; i < TABLE_SIZE; i++)
         hash_table[i] = NULL;
@@ -34,15 +34,13 @@ void print_table_status()
     printf("\t--START--\n");
     for (int i = 0; i < TABLE_SIZE; i++)
     {
-        if (hash_table[i] == NULL)
-        {
+        if (!hash_table[i])
             printf("\t%i\t---\n", i);
-        }
         else
         {
             printf("\t%i\t", i);
             User *tmp = hash_table[i];
-            while (tmp != NULL)
+            while (tmp)
             {
                 printf("%s -> ", tmp->username);
                 tmp = tmp->next;
@@ -58,10 +56,8 @@ User *hash_table_lookup(char *username)
 {
     int key = hash(username);
     User *tmp = hash_table[key];
-    while (tmp != NULL && strcmp(tmp->username, username) != 0)
-    {
+    while (tmp && strcmp(tmp->username, username) != 0)
         tmp = tmp->next;
-    }
     return tmp;
 }
 
@@ -70,28 +66,24 @@ User *hash_table_delete(char *username)
     int key = hash(username);
     User *tmp = hash_table[key];
     User *prev = NULL;
-    while (tmp != NULL && strcmp(tmp->username, username) != 0)
+    while (tmp && strcmp(tmp->username, username) != 0)
     {
         prev = tmp;
         tmp = tmp->next;
     }
-    if (tmp == NULL)
+    if (!tmp)
         return NULL;
-    if (prev == NULL)
-    {
+    if (!prev)
         // deleting the head of the hashtable
         hash_table[key] = tmp->next;
-    }
     else
-    {
         prev->next = tmp->next; // free tmp if using heap
-    }
     return tmp;
 }
 
 bool valid_user(User * u)
 {
-    if (u == NULL)
+    if (!u)
         return false;
     if ((strcmp(u->gender, "M") && strcmp(u->gender, "F") != 0))
         return false;
@@ -107,7 +99,7 @@ bool valid_user(User * u)
 
 bool insert_user(User * u)
 {
-    if (valid_user(u) == true)
+    if (valid_user(u))
     {
         int key = g_string_hash(&u.username);//hash(u->username); 
         u->next = hash_table[key]; // using a linked list to prevent colisions
@@ -118,17 +110,36 @@ bool insert_user(User * u)
         return false;
 }
 
-void *parse(const char *file_name)
+void *parse_user(const char *file_name)
 {
     FILE *open = NULL;
     open = fopen(file_name, "r");
+    char *info;
+    int line = 0;
+    int count = 0;
     User u;
     if (open == NULL)
         printf("O respetivo ficheiro está vazio.\n");
-    while ((fscanf(open,"%s;%s;%s;%s;%s;%s;%s\n",&u.username,&u.name,&u.gender,&u.birth_date,&u.account_creation,&u.pay_method,&u.account_status)) != EOF)
+    while ((fgets(info,sizeof(info),open)){
+          line ++;
+          char *field = strtok(info,";");                           // alternativa usando o 'fgets' que, pelos vistos, é mais aconselhado
+          while (field)
+          {
+            if(count == 0) strcpy(u.username,field);
+            if(count == 1) strcpy(u.name,field);
+            if(count == 2) strcpy(u.gender,field);
+            if(count == 3) strcpy(u.birth_date,field);
+            if(count == 4) strcpy(u.account_creation,field);
+            if(count == 5) strcpy(u.pay_method,field);
+            if(count == 6) strcpy(u.account_status,field);
+            field = strtok(NULL,";");
+            count ++;
+          }    
+    }
+    /*while ((fscanf(open,"%s;%s;%s;%s;%s;%s;%s\n",&u.username,&u.name,&u.gender,&u.birth_date,&u.account_creation,&u.pay_method,&u.account_status)) != EOF)
     {
         insert_user(&user);
-    }
+    }*/
     fclose(open); /*users_hashtable já preenchida*/;
 }
 
